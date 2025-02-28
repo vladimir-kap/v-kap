215ad0f870a9958db790e65d6384c6f846124b4e
vladimir-kap <vladimir362616@gmail.com>
2025-03-01 01:05:52 +0300

    rework of webpages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Страница загружена!');
+    const hoverElements = document.querySelectorAll('.hover-effect');
+
+    hoverElements.forEach(element => {
+        element.addEventListener('mouseover', function() {
+            this.style.color = '#ff9800';
+        });
+
+        element.addEventListener('mouseout', function() {
+            this.style.color = '#e0e0e0';
+        });
+    });
 });
