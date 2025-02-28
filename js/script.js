a21cc6c489029050cab924b84a1cdacba21e5592
vladimir-kap <vladimir362616@gmail.com>
2025-03-01 01:32:11 +0300

    changed styles and markup

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
+# !script.js
+
 document.addEventListener('DOMContentLoaded', function() {
     const hoverElements = document.querySelectorAll('.hover-effect');
 
     hoverElements.forEach(element => {
         element.addEventListener('mouseover', function() {
-            this.style.color = '#ff9800';
+            this.style.color = '#ffeb3b';
         });
 
         element.addEventListener('mouseout', function() {
-            this.style.color = '#e0e0e0';
+            this.style.color = '#ffffff';
         });
     });
 });
